3815f13631824a858bc6fd5d680c7fd50ef9071c
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2025-01-02 13:41:54 +0100

    MOD Vista de bienvenida

--- app/Views/v_bienvenida.php
+++ app/Views/v_bienvenida.php
@@ -4,31 +4,64 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Bienvenidos</title>
-        <link rel="stylesheet" href="<?= base_url('css/styleBienvenida.css'); ?>">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
         <div class="container mt-5">
             <div class="row">
                 <div class="col-md-8 offset-md-2 text-center">
-                    <h1 class="display-4">Bienvenido de nuevo <?= session()->get('cliente')->nombre; ?></h1>
-                    <p class="lead">Your trusted partner for convenient and reliable bus transportation.</p>
+                    <h1 class="display-4">Bienvenido de nuevo <strong><?= session()->get('cliente')->nombre; ?></strong></h1>
+                    <!-- <p class="lead">Tu compañero de confianza para un transporte cómodo y seguro.</p> -->
                 </div>
+            </div>
+
+
+            <div class="row g-4">
+                <!-- Reserva de Billetes -->
+                <div class="col-md-4">
+                    <div class="card h-100">                       
+                        <img src="<?= base_url('/images/reservaOnline.png')?>" class="card-img-top" style="height: 150px;">
+                        <div class="card-body">
+                            <h3 class="card-title">Reserva de Billetes</h3>
+                            <p class="card-text">
+                                Reserva tus billetes de manera fácil/rápida
+                            </p>
+                            <a href="#" class="btn btn-primary">Reservar Ahora</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Consulta de Horarios -->
+                <div class="col-md-4">
+                    <div class="card h-100">
+                        <img src="<?= base_url('/images/rutas.png')?>" class="card-img-top" style="height: 150px;">
+                        <div class="card-body">
+                            <h3 class="card-title">Horarios y Rutas</h3>
+                            <p class="card-text">
+                                Accede a toda la información de viajes
+                            </p>
+                            <a href="#" class="btn btn-primary">Ver Horarios</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Tarifas -->
+                <div class="col-md-4">
+                    <div class="card h-100">
+                        <img src="<?= base_url('/images/precio.png')?>" class="card-img-top" style="height: 150px;">
+                        <div class="card-body">
+                            <h3 class="card-title">Tarifas</h3>
+                            <p class="card-text">
+                                Consulta las tarifas de todas la lineas
+                            </p>
+                            <a href="#" class="btn btn-primary">Consultar Tarifas</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
 
-
-
-        <div id="divPrin">
-            <h1>Bienvenido de nuevo, <?= session()->get('cliente')->nombre; ?> </h1>
-            AQUI luego pongo los servicios que tendra el cliente con igual fotos descriptivas en formato cartas o algo asi ... 'los servicios q ofrece la pagina al cliente'
-            y si es un Admin pues tendra otras cosas destintas pero seria el mismo concepto 
-            y claro voy a arreglar el css tambn 
-            Y si es un visitante sin session otras cosas tmb
-
-        </div>
-
-
-        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
-
